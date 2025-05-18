9ad9780e2269a113c1d7d4c5e17e1c68b57d1598
Serhii <sir3720@gmail.com>
2025-05-18 23:54:17 +0300

    add html

--- src/main.js
+++ src/main.js
@@ -8,3 +8,48 @@ import {
   hideLoader,
   clearGallery,
 } from './js/render-functions';
+
+// 🔍 DOM-елементи
+const form = document.querySelector('.form');
+const gallery = document.querySelector('.gallery');
+const loader = document.querySelector('.loader'); // переконайся, що є елемент з класом loader
+
+form.addEventListener('submit', async event => {
+  event.preventDefault();
+  const query = event.currentTarget.elements['search-text'].value.trim();
+
+  if (!query) {
+    iziToast.warning({
+      title: 'Warning',
+      message: 'Please enter a search query!',
+      position: 'topRight',
+    });
+    return;
+  }
+
+  clearGallery(gallery);
+  showLoader(loader);
+
+  try {
+    const images = await getImagesByQuery(query);
+
+    if (images.length === 0) {
+      iziToast.error({
+        title: 'No Results',
+        message: 'No images found. Try a different search.',
+        position: 'topRight',
+      });
+    } else {
+      createGallery(images, gallery);
+    }
+  } catch (error) {
+    iziToast.error({
+      title: 'Error',
+      message: 'Something went wrong. Please try again later.',
+      position: 'topRight',
+    });
+    console.error(error);
+  } finally {
+    hideLoader(loader);
+  }
+});
